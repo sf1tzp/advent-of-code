c8e960eed7812913f1fd25ac7101682c08dfeff4
Steven Fitzpatrick <steven@fitz.gg>
2022-12-02 01:54:03 -0600

    Day 1

---
+++ src/bin/day-1.rs
@@ -0,0 +1,53 @@
+type Calories = isize;
+
+#[derive(Debug)]
+struct Elf {
+    food: Vec<Calories>,
+}
+
+fn main() {
+    let input = advent_of_code_2022::read_input().expect("failed to read input");
+    let inventory = take_inventory(input);
+
+    let mut calorie_counts = inventory
+        .iter()
+        .map(|elf| elf.food.iter().sum())
+        .collect::<Vec<Calories>>();
+
+    calorie_counts.sort_by(|a, b| b.cmp(a));
+
+    let max = calorie_counts[0];
+    println!("(#1) Most Calories: {max}");
+
+    let top_3 = &calorie_counts[0..3];
+    let top_3_sum: Calories = top_3.iter().sum();
+    println!(
+        "(#2) The Top 3 Have {:?} and Their Sum is {}",
+        top_3, top_3_sum
+    )
+}
+
+// Split the input on blank lines, then parse each elf's inventory
+fn take_inventory(mut input: String) -> Vec<Elf> {
+    // Remove Trailing Newline
+    if input.ends_with('\n') {
+        input.pop();
+    }
+
+    // Can't decide how I feel about this code. But it was fun to write, kinda :)
+    let inventory: Vec<Elf> = input
+        .split("\n\n")
+        .map(|inv| Elf {
+            food: inv
+                .split('\n')
+                .map(|food| {
+                    food.parse::<Calories>()
+                        .expect(&format!("failed to parse calories from {}", food))
+                })
+                .collect(),
+        })
+        .collect();
+
+    println!("Took inventories of {} elves", inventory.len());
+    inventory
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,17 @@
 use std::{
     env,
     fs::File,
-    io::{BufReader, Read},
+    io::{self, BufReader, Read},
 };
 
-pub fn read_input() -> String {
+pub fn read_input() -> io::Result<String> {
     let input_path = env::var("INPUT_PATH").expect("INPUT_PATH not defined");
     let file = File::open(&input_path).expect(&format!("Input file {} not found", input_path));
 
     let mut reader = BufReader::new(file);
     let mut input = String::new();
 
-    reader
-        .read_to_string(&mut input)
-        .expect("Input file was empty");
+    reader.read_to_string(&mut input)?;
 
-    input
+    Ok(input)
 }
