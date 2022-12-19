4d5f30b51b888b6877c64828ee6ebe6ad9fc3e1a
Steven Fitzpatrick <steven@fitz.gg>
2022-12-19 01:24:43 -0600

    Day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -2,20 +2,28 @@ use std::collections::{HashSet, VecDeque};
 
 static _EASY: &str = "bvwbjplbgvbhsrlpgdmjqwftvncz"; // Should result in 5
 
+#[derive(Clone, Copy)]
+enum Marker {
+    PacketStart = 4,
+    MessageStart = 14,
+}
+
 struct Identifier {
+    class: Marker,
     buffer: VecDeque<char>,
 }
 
 impl Identifier {
-    fn new() -> Identifier {
+    fn new(class: Marker) -> Identifier {
         Identifier {
+            class,
             buffer: VecDeque::new(),
         }
     }
     
     fn push(&mut self, item: char) {
         self.buffer.push_back(item);
-        if self.buffer.len() > 4 {
+        if self.buffer.len() > self.class as usize {
             self.buffer.pop_front();
         }
     }
@@ -23,7 +31,7 @@ impl Identifier {
     fn check_for_start_of_packet(&self) -> bool {
         // println!("checking buffer {:?}", self.buffer);
         let mut seen: HashSet<char> = HashSet::new();
-        if self.buffer.len() != 4 {
+        if self.buffer.len() != self.class as usize {
             return false;
         }
         for c in self.buffer.iter() {
@@ -37,10 +45,9 @@ impl Identifier {
     }
 }
 
-fn find_packet(input: &str) -> Option<usize> {
-    let mut id = Identifier::new();
+fn find_marker(input: &str, class: Marker) -> Option<usize> {
+    let mut id = Identifier::new(class);
     for (i, c) in input.chars().enumerate() {
-        println!("{} {}", i, c);
         id.push(c);
         if id.check_for_start_of_packet() {
             return Some(i);
@@ -53,5 +60,10 @@ fn find_packet(input: &str) -> Option<usize> {
 #[aoc(day6, part1)]
 fn solve_part1(input: &str) -> usize {
     // let input = EASY;
-    find_packet(input).unwrap() + 1
+    find_marker(input, Marker::PacketStart).unwrap() + 1
+}
+
+#[aoc(day6, part2)]
+fn solve_part2(input: &str) -> usize {
+    find_marker(input, Marker::MessageStart).unwrap() + 1
 }
