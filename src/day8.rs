f165dd88eefd9cd466657ff55af45e88da016961
Steven Fitzpatrick <steven@fitz.gg>
2022-12-23 03:23:24 -0600

    Day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -52,7 +52,7 @@ fn parse_input(input: &str) -> TreeFarm {
 }
 
 #[aoc(day8, part1)]
-fn count_visible(input: &TreeFarm) -> usize {
+fn solve_part1(input: &TreeFarm) -> usize {
     let mut input: TreeFarm = input.clone();
     // println!("{}", input);
     for row_index in 0..=input.size.rows {
@@ -67,6 +67,20 @@ fn count_visible(input: &TreeFarm) -> usize {
     let count = input.count_visible_trees();
     // println!("{:?}", input);
     count
+}
+
+#[aoc(day8, part2)]
+fn solve_part2(input: &TreeFarm) -> usize {
+    // println!("{}", input);
+    let mut high_score = 0;
+    for (location, _) in input.plots.iter() {
+        let score = input.base_visibility_score(location);
+        if high_score < score {
+            high_score = score;
+        }
+    }
+
+    high_score
 }
 
 impl fmt::Display for TreeFarm {
@@ -142,6 +156,67 @@ impl TreeFarm {
                 self.plots.get_mut(&location).unwrap().visible_from_outside = true;
             }
         }
+    }
+
+    pub fn base_visibility_score(&self, location: &(usize, usize)) -> usize {
+        let base_row = location.0;
+        let base_column = location.1;
+        let base_height = self.plots[location].height;
+        let mut score = 1; // Start at 1, we will *= this in the loops
+
+        // If the base location is on the edge of the forest, the score is 0
+        if base_row == 0
+            || base_row == self.size.rows
+            || base_column == 0
+            || base_column == self.size.columns
+        {
+            return 0;
+        }
+
+        // Iterate from the spaces adjacent to the base to the edge
+
+        // Score Up (Base.Row -1 -> Row 0)
+        let range = (0..=(base_row - 1)).rev();
+        for (i, row_index) in range.enumerate() {
+            if self.plots[&(row_index, base_column)].height >= base_height || row_index == 0 {
+                score *= i + 1;
+                break;
+            }
+        }
+
+        // Score Down (Base.Row +1 -> Max Row)
+        let range = (base_row + 1)..=self.size.rows;
+        for (i, row_index) in range.enumerate() {
+            if self.plots[&(row_index, base_column)].height >= base_height
+                || row_index == self.size.rows
+            {
+                score *= i + 1;
+                break;
+            }
+        }
+
+        // Score Left (Base -> Row 0)
+        let range = (0..=(base_column - 1)).rev();
+        for (i, column_index) in range.enumerate() {
+            if self.plots[&(base_row, column_index)].height >= base_height || column_index == 0 {
+                score *= i + 1;
+                break;
+            }
+        }
+
+        // Score Right (Base -> Max Column)
+        let range = (base_column + 1)..=self.size.columns;
+        for (i, column_index) in range.enumerate() {
+            if self.plots[&(base_row, column_index)].height >= base_height
+                || column_index == self.size.columns
+            {
+                score *= i + 1;
+                break;
+            }
+        }
+
+        // println!("location {:?} score {}", location, score);
+        score
     }
 }
 
