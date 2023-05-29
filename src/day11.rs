701cd56e059bbaf7c25b616d78dc47597d4e152a
Steven Fitzpatrick <steven@fitz.gg>
2023-05-29 01:58:59 -0600

    Day 11 Part 1

--- src/day11.rs
+++ src/day11.rs
@@ -3,6 +3,7 @@ use std::collections::{HashMap, VecDeque};
 use regex::Regex;
 
 use anyhow::{anyhow, Result};
+use itertools::Itertools; // itertools = "0.8"
 
 #[derive(Debug, Clone)]
 enum Operation {
@@ -17,7 +18,7 @@ enum Magnitude {
 }
 
 #[derive(Debug, Clone)]
-struct Thrower {
+struct Monkey {
     items: VecDeque<usize>,
     operation: Operation,
     magnitude: Magnitude,
@@ -26,81 +27,87 @@ struct Thrower {
     false_target: usize,
 }
 
-// #[aoc_generator(day11)]
-fn parse_input(input: &str) -> HashMap<usize, Thrower> {
-    let mut monkeys = HashMap::<usize, Thrower>::new();
-    let mut lines = input.lines().peekable();
+impl Monkey {
+    fn run_turn(&mut self) -> VecDeque<(usize, usize)> {
+        let mut item_updates = VecDeque::<(usize, usize)>::new();
+        while !self.items.is_empty() {
+            let item_score = self.items.pop_front().unwrap();
+            let item_score = match self.operation {
+                Operation::Add => match self.magnitude {
+                    Magnitude::Factor(n) => item_score + n,
+                    Magnitude::SameAsBefore => item_score + item_score,
+                },
+                Operation::Multiply => match self.magnitude {
+                    Magnitude::Factor(n) => item_score * n,
+                    Magnitude::SameAsBefore => item_score * item_score,
+                },
+            };
+            let item_score = (item_score as f64 / 3.0).floor() as usize;
 
-    while lines.peek().is_some() {
-        let id = capture_numbers(lines.next().unwrap()).unwrap()[0];
-        let items = capture_numbers(lines.next().unwrap()).unwrap();
-        let (operation, magnitude) = capture_operation(lines.next().unwrap()).unwrap();
-        let test_value = capture_numbers(lines.next().unwrap()).unwrap()[0];
-        let true_target = capture_numbers(lines.next().unwrap()).unwrap()[0];
-        let false_target = capture_numbers(lines.next().unwrap()).unwrap()[0];
-        let monkey = Thrower {
-            items: items.into_iter().collect(),
-            operation,
-            magnitude,
-            test_value,
-            true_target,
-            false_target,
-        };
-        monkeys.insert(id, monkey);
+            let target = match item_score % self.test_value == 0 {
+                true => self.true_target,
+                false => self.false_target,
+            };
 
-        let _ = lines.next(); // Skip the blank line separating input blocks
-    }
-
-    monkeys
-}
-
-#[aoc(day11, part1)]
-fn solve_part1(input: &str) -> usize {
-    let mut monkeys = parse_input(input);
-    run_round(&mut monkeys);
-    0
-}
-
-fn run_round(monkeys: &mut HashMap<usize, Thrower>) {
-    println!("Round start {:?}", monkeys);
-    for i in 0..monkeys.len() {
-        run_turn(i, monkeys)
-    }
-    println!("Round end {:?}", monkeys);
-}
-
-fn run_turn(id: usize, monkeys: &mut HashMap<usize, Thrower>) {
-    let mut updates = VecDeque::<(usize, usize)>::new();
-    let monkey = monkeys.get_mut(&id).unwrap();
-    while !monkey.items.is_empty() {
-        let item_score = monkey.items.pop_front().unwrap();
-        let item_score = match monkey.operation {
-            Operation::Add => match monkey.magnitude {
-                Magnitude::Factor(n) => item_score + n,
-                Magnitude::SameAsBefore => item_score + item_score,
-            },
-            Operation::Multiply => match monkey.magnitude {
-                Magnitude::Factor(n) => item_score * n,
-                Magnitude::SameAsBefore => item_score * item_score,
-            },
-        };
-        let item_score = (item_score as f64 / 3.0).floor() as usize;
-
-        let target = match item_score % monkey.test_value == 0 {
-            true => monkey.true_target,
-            false => monkey.false_target,
-        };
-
-        updates.push_back((target, item_score));
-    }
-
-    while !updates.is_empty() {
-        let (target, item_score) = updates.pop_front().unwrap();
-        let monkey = monkeys.get_mut(&target).unwrap();
-        monkey.items.push_back(item_score);
+            item_updates.push_back((target, item_score));
+        }
+        item_updates
     }
 }
 
+#[derive(Debug, Clone)]
+struct MonkeyBusinessTracker {
+    monkeys: HashMap<usize, Monkey>,
+    inspection_counts: HashMap<usize, usize>,
+}
+
+impl MonkeyBusinessTracker {
+    fn new_from_input(monkeys: HashMap<usize, Monkey>) -> Self {
+        MonkeyBusinessTracker {
+            monkeys,
+            inspection_counts: HashMap::<usize, usize>::new(),
+        }
+    }
+
+    fn update_activity(&mut self, monkey_id: usize, count: usize) {
+        self.inspection_counts
+            .entry(monkey_id)
+            .and_modify(|counter| *counter += count)
+            .or_insert(count);
+    }
+
+    fn update_items(&mut self, updates: VecDeque<(usize, usize)>) {
+        for (monkey_id, item_score) in updates {
+            let target_monkey = self.monkeys.get_mut(&monkey_id).unwrap();
+            target_monkey.items.push_back(item_score);
+        }
+    }
+
+    fn print_items(&self) {
+        for (monkey_id, monkey) in self.monkeys.iter() {
+            println!("Monkey {}: {:?}", monkey_id, monkey.items);
+        }
+    }
+
+    fn print_inspections(&self) {
+        for monkey_id in self.inspection_counts.keys().sorted() {
+            println!("Monkey {} has inspected {:?} items", monkey_id, self.inspection_counts.get(monkey_id).unwrap());
+        }
+    }
+
+    fn calculate(&self, n: usize) -> usize {
+        // Find the ids of the top two inspection counts
+        self.inspection_counts
+            .values()
+            .sorted()
+            .rev()
+            .take(n)
+            .copied()
+            .product()
+    }
+}
+
+// Parse a list of numbers from a string
 fn capture_numbers(line: &str) -> Result<Vec<usize>> {
     let re = Regex::new(r"(\d+)").unwrap();
     let items: Vec<usize> = re
@@ -111,6 +118,7 @@ fn capture_numbers(line: &str) -> Result<Vec<usize>> {
     Ok(items)
 }
 
+// Parse an Operation from a string
 fn capture_operation(line: &str) -> Result<(Operation, Magnitude)> {
     let re = Regex::new(r"(\*|\+) (old|\d+)").unwrap();
     let captures = re.captures(line).unwrap();
@@ -138,4 +146,52 @@ fn capture_operation(line: &str) -> Result<(Operation, Magnitude)> {
     };
 
     Ok((operation, magnitude))
+}
+
+#[aoc_generator(day11)]
+fn parse_input(input: &str) -> HashMap<usize, Monkey> {
+    let mut monkeys = HashMap::<usize, Monkey>::new();
+    let mut lines = input.lines().peekable();
+
+    while lines.peek().is_some() {
+        let id = capture_numbers(lines.next().unwrap()).unwrap()[0];
+        let items = capture_numbers(lines.next().unwrap()).unwrap();
+        let (operation, magnitude) = capture_operation(lines.next().unwrap()).unwrap();
+        let test_value = capture_numbers(lines.next().unwrap()).unwrap()[0];
+        let true_target = capture_numbers(lines.next().unwrap()).unwrap()[0];
+        let false_target = capture_numbers(lines.next().unwrap()).unwrap()[0];
+        let monkey = Monkey {
+            items: items.into_iter().collect(),
+            operation,
+            magnitude,
+            test_value,
+            true_target,
+            false_target,
+        };
+        monkeys.insert(id, monkey);
+
+        let _ = lines.next(); // Skip the blank line separating input blocks
+    }
+
+    monkeys
+}
+
+#[aoc(day11, part1)]
+fn solve_part1(input: &HashMap<usize, Monkey>) -> usize {
+    let mut tracker = MonkeyBusinessTracker::new_from_input(input.clone());
+    tracker.print_items();
+    // run 20 rounds
+    for i in 0..20 {
+        println!("Round {}", i + 1);
+        for monkey_id in 0..tracker.monkeys.len() {
+            let item_count = tracker.monkeys.get(&monkey_id).unwrap().items.len();
+            tracker.update_activity(monkey_id, item_count);
+            let monkey = tracker.monkeys.get_mut(&monkey_id).unwrap();
+            let updates = monkey.run_turn();
+            tracker.update_items(updates);
+        }
+        tracker.print_items();
+    }
+    tracker.print_inspections();
+    tracker.calculate(2)
 }
