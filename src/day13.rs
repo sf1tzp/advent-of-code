ebcd6744149167a6666ff4a37140ca54ae0fc95f
Steven Fitzpatrick <steven@fitz.gg>
2023-05-31 14:32:43 -0700

    Day 13 part 1

--- src/day13.rs
+++ src/day13.rs
@@ -1,4 +1,5 @@
 use std::collections::VecDeque;
+use std::cmp::Ordering;
 
 use anyhow::{anyhow, Result};
 
@@ -8,9 +9,91 @@ enum Data {
     List(Vec<Data>),
 }
 
+impl Ord for Data {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let lhs = self;
+        let rhs = other;
+        match (self, other) {
+            (Data::Number(l), Data::Number(r)) => l.cmp(r),
+            (Data::List(l), Data::List(r)) => {
+                let mut l_iter = l.iter();
+                let mut r_iter = r.iter();
+                loop {
+                    let (l, r) = (l_iter.next(), r_iter.next());
+                    match (l, r) {
+                        (Some(l), Some(r)) => {
+                            let cmp = l.cmp(r);
+                            if cmp != Ordering::Equal {
+                                return cmp;
+                            }
+                        }
+                        (Some(_), None) => {
+                            return Ordering::Greater;
+                        }
+                        (None, Some(_)) => {
+                            return Ordering::Less;
+                        }
+                        (None, None) => {
+                            return Ordering::Equal;
+                        }
+                    }
+                }
+            },
+            (Data::Number(l), Data::List(_)) => {
+                let lhs = Data::List(vec![Data::Number(*l)]);
+                lhs.cmp(rhs)
+            }
+            (Data::List(_), Data::Number(r)) => {
+                let rhs = Data::List(vec![Data::Number(*r)]);
+                lhs.cmp(&rhs)
+            }
+        }
+    }
+}
+
+impl PartialOrd for Data {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct Packet {
     data: Vec<Data>,
+}
+
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let lhs = &self.data;
+        let rhs = &other.data;
+        let mut l_iter = lhs.iter();
+        let mut r_iter = rhs.iter();
+        loop {
+            let (l, r) = (l_iter.next(), r_iter.next());
+            match (l, r) {
+                (Some(l), Some(r)) => {
+                    let cmp = l.cmp(r);
+                    if cmp != Ordering::Equal {
+                        return cmp;
+                    }
+                }
+                (Some(_), None) => {
+                    return Ordering::Greater;
+                }
+                (None, Some(_)) => {
+                    return Ordering::Less;
+                }
+                (None, None) => {
+                    return Ordering::Equal;
+                }
+            }
+        }
+    }
+}
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl Packet {
@@ -124,16 +207,20 @@ fn input_generator(input: &str) -> Vec<(Packet, Packet)> {
 }
 
 #[aoc(day13, part1)]
-fn solve_part1(packets: &Vec<(Packet, Packet)>) -> usize {
-    for (lhs, rhs) in packets {
-        println!("lhs: {:?}", lhs);
-        println!("rhs: {:?}", rhs);
+fn solve_part1(packets: &[(Packet, Packet)]) -> usize {
+    let mut count = 0;
+    for (i, (lhs, rhs)) in packets.iter().enumerate() {
+        if lhs < rhs {
+            count += i + 1;
+        }
     }
-    0
+    count
 }
 
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
     use super::*;
 
     #[test]
@@ -261,5 +348,179 @@ mod tests {
         ];
         let result = input_generator(input);
         assert_eq!(result, expected, "Packets should match");
+    }
+
+    #[test]
+    fn test_ord_for_data() {
+        struct TestCase {
+            description: &'static str,
+            lhs: Data,
+            rhs: Data,
+            expected: Ordering,
+        }
+
+        let cases = vec![
+            TestCase {
+                description: "Number < Number",
+                lhs: Data::Number(1),
+                rhs: Data::Number(2),
+                expected: Ordering::Less,
+            },
+            TestCase {
+                description: "Number > Number",
+                lhs: Data::Number(2),
+                rhs: Data::Number(1),
+                expected: Ordering::Greater,
+            },
+            TestCase {
+                description: "Number == Number",
+                lhs: Data::Number(1),
+                rhs: Data::Number(1),
+                expected: Ordering::Equal,
+            },
+            TestCase {
+                description: "Equal Lists",
+                lhs: Data::List(vec![Data::Number(1)]),
+                rhs: Data::List(vec![Data::Number(1)]),
+                expected: Ordering::Equal,
+            },
+            TestCase {
+                description: "List < List",
+                lhs: Data::List(vec![Data::Number(1)]),
+                rhs: Data::List(vec![Data::Number(2)]),
+                expected: Ordering::Less,
+            },
+            TestCase {
+                description: "List > List",
+                lhs: Data::List(vec![Data::Number(2)]),
+                rhs: Data::List(vec![Data::Number(1)]),
+                expected: Ordering::Greater,
+            },
+            TestCase {
+                description: "Shorter List < Longer List",
+                lhs: Data::List(vec![Data::Number(1)]),
+                rhs: Data::List(vec![Data::Number(1), Data::Number(2)]),
+                expected: Ordering::Less,
+            },
+            TestCase {
+                description: "Longer List > Shorter List",
+                lhs: Data::List(vec![Data::Number(1), Data::Number(2)]),
+                rhs: Data::List(vec![Data::Number(1)]),
+                expected: Ordering::Greater,
+            },
+            TestCase {
+                description: "Number == One element list",
+                lhs: Data::Number(1),
+                rhs: Data::List(vec![Data::Number(1)]),
+                expected: Ordering::Equal,
+            },
+            TestCase {
+                description: "Number < Two element list",
+                lhs: Data::Number(1),
+                rhs: Data::List(vec![Data::Number(1), Data::Number(2)]),
+                expected: Ordering::Less,
+            },
+            TestCase {
+                description: "Number > Two element list",
+                lhs: Data::Number(3),
+                rhs: Data::List(vec![Data::Number(1), Data::Number(2)]),
+                expected: Ordering::Greater,
+            },
+            TestCase {
+                description: "Empty List < List",
+                lhs: Data::List(vec![]),
+                rhs: Data::List(vec![Data::Number(1)]),
+                expected: Ordering::Less,
+            },
+            TestCase {
+                description: "Empty List < Number ",
+                lhs: Data::List(vec![]),
+                rhs: Data::Number(1),
+                expected: Ordering::Less,
+            },
+        ];
+
+        for case in cases {
+            let result = case.lhs.cmp(&case.rhs);
+            assert_eq!(result, case.expected, "{}", case.description);
+        }
+    }
+
+    #[test]
+    fn test_ord_for_packet() {
+        struct TestCase {
+            description: &'static str,
+            lhs: &'static str,
+            rhs: &'static str,
+            expected: Ordering,
+        }
+        let cases = vec![
+            TestCase {
+                description: "Equal Packets",
+                lhs: "[1]",
+                rhs: "[1]",
+                expected: Ordering::Equal,
+            },
+            TestCase {
+                description: "Packet < Packet",
+                lhs: "[1]",
+                rhs: "[2]",
+                expected: Ordering::Less,
+            },
+            TestCase {
+                description: "Example 1",
+                lhs: "[1,1,3,1,1]",
+                rhs: "[1,1,5,1,1]",
+                expected: Ordering::Less,
+            },
+            TestCase {
+                description: "Example 2",
+                lhs: "[[1],[2,3,4]]",
+                rhs: "[[1],4]",
+                expected: Ordering::Less,
+            },
+            TestCase {
+                description: "Example 3",
+                lhs: "[9]",
+                rhs: "[[8,7,6]]",
+                expected: Ordering::Greater,
+            },
+            TestCase {
+                description: "Example 4",
+                lhs: "[[4,4],4,4]",
+                rhs: "[[4,4],4,4,4]",
+                expected: Ordering::Less,
+            },
+            TestCase {
+                description: "Example 5",
+                lhs: "[7,7,7,7]",
+                rhs: "[7,7,7]",
+                expected: Ordering::Greater,
+            },
+            TestCase {
+                description: "Example 6",
+                lhs: "[]",
+                rhs: "[3]",
+                expected: Ordering::Less,
+            },
+            TestCase {
+                description: "Example 7",
+                lhs: "[[[]]]",
+                rhs: "[[]]",
+                expected: Ordering::Greater,
+            },
+            TestCase {
+                description: "Example 8",
+                lhs: "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+                rhs: "[1,[2,[3,[4,[5,6,0]]]],8,9]",
+                expected: Ordering::Greater,
+            },
+        ];
+        for case in cases {
+            let lhs = Packet::read(case.lhs).unwrap();
+            let rhs = Packet::read(case.rhs).unwrap();
+            let result = lhs.cmp(&rhs);
+            assert_eq!(result, case.expected, "{}", case.description);
+        }
     }
 }
